f1dfcacbde2a2022bf6b3874108aa74985a06c12
Alano Martins <alanomartinspinto@gmail.com>
2018-06-04 19:36:46 -0300

    Inicial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+
+from sklearn import svm
+
+from sklearn.covariance import EllipticEnvelope
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+
+path = "dataset/covtypeNorm.csv"
+data = pd.read_csv(path, header=None)
+y_data = data.iloc[:,54]
+
+cl1 = data.loc[data[54] == 1]
+cl2 = data.loc[data[54] == 2]
+cl3 = data.loc[data[54] == 3]
+cl4 = data.loc[data[54] == 4]
+cl5 = data.loc[data[54] == 5]
+cl6 = data.loc[data[54] == 6]
+cl7 = data.loc[data[54] == 7]
+
+cl1.drop(cl1.columns[54], axis=1, inplace=True)
+cl2.drop(cl2.columns[54], axis=1, inplace=True)
+cl3.drop(cl3.columns[54], axis=1, inplace=True)
+cl4.drop(cl4.columns[54], axis=1, inplace=True)
+cl5.drop(cl5.columns[54], axis=1, inplace=True)
+cl6.drop(cl6.columns[54], axis=1, inplace=True)
+cl7.drop(cl7.columns[54], axis=1, inplace=True)
+cls = [cl1, cl2, cl3, cl4, cl5, cl6, cl7]
+
+data.drop(data.columns[54], axis=1, inplace=True)
+
+# fit the model
+clf = LocalOutlierFactor(n_neighbors=10, n_jobs=-1)
+
+new_data = np.concatenate((cl1, cl1), axis=0)
+clf.fit(new_data)
+y_pred = clf.fit_predict(cl1)
+print(y_pred)
